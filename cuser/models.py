3ccb1ce7e9e677d723ac6678d203b238b5b523b0
Thomas Meagher <thom.meagher@gmail.com>
2016-01-18 13:39:16 -0500

    Custom user model complete

--- cuser/models.py
+++ cuser/models.py
@@ -1,71 +1,109 @@
 from django.db import models
 from django.contrib.auth.models import (
-    BaseUserManager, AbstractBaseUser
+    BaseUserManager, PermissionsMixin, AbstractBaseUser
 )
+from django.core import validators
+from django.core.mail import send_mail
+from django.utils import timezone
 
 
 class CUserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
         """
-        Creates and saves a User with the given email and password.
+        Creates and saves a User with the given username, email and password.
         """
         if not email:
-            raise ValueError('Users must have an email address')
-
-        user = self.model(email=self.normalize_email(email), **extra_fields)
-
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
     def create_superuser(self, email, password, **extra_fields):
-        """
-        Creates and saves a superuser with the given email and password.
-        """
-        user = self.create_user(email, password=password, **extra_fields)
-        user.is_admin = True
-        user.save(using=self._db)
-        return user
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
 
 
-class CUser(AbstractBaseUser):
+class AbstractCUser(AbstractBaseUser, PermissionsMixin):
+    """
+    An abstract base class implementing a fully featured User model with
+    admin-compliant permissions.
+    Email and password are required. Other fields are optional.
+    """
     email = models.EmailField(
-        verbose_name='email address',
+        'email',
         max_length=255,
         unique=True,
+        help_text='Required. 255 characters or fewer. Letters, digits, and @/./+/-/_ only.',
+        validators=[
+            validators.EmailValidator(),
+        ],
+        error_messages={
+            'unique': "A user with that email address already exists.",
+        },
     )
     first_name = models.CharField('first name', max_length=30, blank=True)
     last_name = models.CharField('last name', max_length=30, blank=True)
-    is_active = models.BooleanField(default=True)
-    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(
+        'staff status',
+        default=False,
+        help_text='Designates whether the user can log into this admin site.',
+    )
+    is_active = models.BooleanField(
+        'active',
+        default=True,
+        help_text='Designates whether this user should be treated as active. Unselect instead of deleting accounts.',
+    )
+    date_joined = models.DateTimeField('date joined', default=timezone.now)
 
     objects = CUserManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+        abstract = True
+
     def get_full_name(self):
+        """
+        Returns the first_name plus the last_name, with a space in between.
+        """
         full_name = '%s %s' % (self.first_name, self.last_name)
         return full_name.strip()
 
     def get_short_name(self):
+        "Returns the short name for the user."
         return self.first_name
 
-    def __str__(self):              # __unicode__ on Python 2
-        return self.email
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        """
+        Sends an email to this User.
+        """
+        send_mail(subject, message, from_email, [self.email], **kwargs)
 
-    def has_perm(self, perm, obj=None):
-        "Does the user have a specific permission?"
-        # Simplest possible answer: Yes, always
-        return True
 
-    def has_module_perms(self, app_label):
-        "Does the user have permissions to view the app `app_label`?"
-        # Simplest possible answer: Yes, always
-        return True
-
-    @property
-    def is_staff(self):
-        "Is the user a member of staff?"
-        # Simplest possible answer: All admins are staff
-        return self.is_admin
+class CUser(AbstractCUser):
+    """
+    Users within the Django authentication system are represented by this
+    model.
+    Username, password and email are required. Other fields are optional.
+    """
+    # class Meta(AbstractCUser.Meta):
+    #     swappable = 'AUTH_USER_MODEL'
